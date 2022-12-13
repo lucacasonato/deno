dd31c164e3a83bb029a5df3f8c41c7093953b7b6
Arnau Orriols <arnauorriolsmiro@gmail.com>
2022-12-13 16:39:09 +0100

    use `Deno.__deployLogToStdout` instead of `console.log`

--- ext/http/01_http.js
+++ ext/http/01_http.js
@@ -376,7 +376,7 @@
           ws[_serverHandleIdleTimeout]();
         }
       } catch(error) {
-        ops.op_deploy_log_to_stdout("[deno][respondWith] " + error);
+        Deno.__deployLogToStdout("[deno] error in respondWith", error);
         throw error;
       } finally {
         if (SetPrototypeDelete(httpConn.managedResources, streamRid)) {
